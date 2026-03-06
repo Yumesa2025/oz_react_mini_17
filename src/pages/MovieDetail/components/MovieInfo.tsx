9adb09f499a67650dc0ac19e558bb20e8f633606
Yumesa2025 <sirtangky2014@gmail.com>
2026-03-06 15:22:33 +0900

    refactor: MovieDetail 로직을 MovieInfo 컴포넌트로 분리

--- src/pages/MovieDetail/components/MovieInfo.tsx
+++ src/pages/MovieDetail/components/MovieInfo.tsx
@@ -1,1 +1,44 @@
-// MovieDetail 페이지 전용 영화 정보 컴포넌트
+import { css } from "styled-system/css";
+import movieDetailData from "../../../moks/movieDetailData.json";
+
+const posterStyle = css({
+  width: "300px",
+  borderRadius: "12px",
+  objectFit: "cover",
+});
+
+const infoStyle = css({
+  display: "flex",
+  flexDirection: "column",
+  gap: "16px",
+  flex: 1,
+});
+
+const headerStyle = css({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+const MovieInfo = () => {
+  const movie = movieDetailData;
+  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  const genreNames = (movie.genres ?? []).map((g) => g.name).join(" , ");
+
+  return (
+    <>
+      {/* 왼쪽: 포스터 */}
+      <img className={posterStyle} src={imageUrl} alt={movie.title} />
+
+      {/* 오른쪽: 정보 */}
+      <div className={infoStyle}>
+        <h2 className={headerStyle}>{movie.title}</h2>
+        <p>평점 {movie.vote_average}</p>
+        <p>{genreNames}</p>
+        <p>{movie.overview}</p>
+      </div>
+    </>
+  );
+};
+
+export default MovieInfo;
--- src/pages/MovieDetail/index.tsx
+++ src/pages/MovieDetail/index.tsx
@@ -1,59 +1,17 @@
-//import { useParams } from "react-router-dom";
-import movieDetailData from "../../moks/movieDetailData.json";
 import { css } from "styled-system/css";
+import MovieInfo from "./components/MovieInfo";
 
-const containerStyle = css ({
+const containerStyle = css({
   display: "flex",
   gap: "32px",
   padding: "32px",
-})
-
-const posterStyle = css({
-  width: "300px",
-  borderRadius: "12px",
-  objectFit: "cover",
 });
-
-const infoStyle = css({
-  display: "flex",
-  flexDirection: "column",
-  gap: "16px",
-  flex: 1,
-});
-
-const headerStyle = css({
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-});
-
 
 const MovieDetail = () => {
-  //const { id } = useParams();
-
-  const movie = movieDetailData;
-  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-  const genreNames = (movie.genres ?? []).map((g) => g.name).join(" , ");
-
   return (
     <div className={containerStyle}>
-       {/* 왼쪽: 포스터 */}
-      <img className={posterStyle}  
-      src={imageUrl} 
-      alt = {movie.title} />
-       {/* 오른쪽: 영화 정보 */}
-       <div className={infoStyle}>
-        <h2 className={headerStyle}>{movie.title}</h2>
-        <p> 평점 {movie.vote_average}</p>
-
-       {/* 장르 */}
-       <p>{genreNames}</p>
-
-       {/* 줄거리 */}
-       <p> {movie.overview} </p>
-       </div>
+      <MovieInfo />
     </div>
   );
 };
-
 export default MovieDetail;
